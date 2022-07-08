1196cc3d8e671bbd93502e26f8672d1d391f0fcb
Shreya Bhatia <Shreya.Bhatia@publicissapient.com>
2022-07-08 10:29:19 +0530

    Rest network test cases added

--- iNewsX/NetworkManager/RestNetworkManager.swift
+++ iNewsX/NetworkManager/RestNetworkManager.swift
@@ -11,10 +11,59 @@ class RestNetworkManager: NSObject, INetworkManager, URLSessionDelegate {
     
     func executeNetworkRequest<T: Codable>(_ type: T.Type, request: BaseRequest, completion: TaskCompletionHandler<T>?) {
         
-        guard let url = URL(string: request.url) else {
-            return
+        do {
+            let urlRequest = try self.createURLRequest(request: request)
+            let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+            let task = session.dataTask(with: urlRequest) { data, response, error in
+                guard error == nil else {
+                    DispatchQueue.main.async {
+                        completion?(.failure(BaseErrorClass(message: error!.localizedDescription)))
+                    }
+                    return
+                }
+                // Conversion
+                guard let data = data else {
+                    DispatchQueue.main.async {
+                        completion?(.failure(BaseErrorClass(message: "Data not found")))
+                    }
+                    return
+                }
+                
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    if statusCode >= 400 {
+                        DispatchQueue.main.async {
+                            completion?(.failure(BaseErrorClass(message: "Network error with status code: \(statusCode)")))
+                        return
+                        }
+                    }
+                }
+                
+                DispatchQueue.main.async {
+                    do {
+                        let model = try JSONDecoder().decode(type, from: data)
+                        completion?(.success(model))
+                    } catch {
+                        completion?(.failure(BaseErrorClass(message: error.localizedDescription)))
+                    }
+                }
+                
+            }
+            task.resume()
+            
+        } catch {
+            DispatchQueue.main.async {
+                completion?(.failure(BaseErrorClass(message: error.localizedDescription)))
+            }
         }
         
+        
+    }
+    
+    func createURLRequest(request: BaseRequest) throws -> URLRequest {
+        
+        guard let url = URL(string: request.url) else {
+            throw BaseErrorClass(message: "Failed to get URL")
+        }
         var urlRequest = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
         urlRequest.httpMethod = request.method.rawValue
         
@@ -23,52 +72,15 @@ class RestNetworkManager: NSObject, INetworkManager, URLSessionDelegate {
                 let bodyData = try JSONSerialization.data(withJSONObject: body, options: [.prettyPrinted])
                 urlRequest.httpBody = bodyData
             } catch {
-                DispatchQueue.main.async {
-                    completion?(.failure(BaseErrorClass(message: error.localizedDescription)))
-                }
+                throw BaseErrorClass(message: error.localizedDescription)
             }
         }
-        
+    
         request.header?.forEach({ key, value in
             urlRequest.setValue(value, forHTTPHeaderField: key)
         })
-        
-        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
-        let task = session.dataTask(with: urlRequest) { data, response, error in
-            guard error == nil else {
-                DispatchQueue.main.async {
-                    completion?(.failure(BaseErrorClass(message: error!.localizedDescription)))
-                }
-                return
-            }
-            // Conversion
-            guard let data = data else {
-                DispatchQueue.main.async {
-                    completion?(.failure(BaseErrorClass(message: "Data not found")))
-                }
-                return
-            }
-            
-            if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                if statusCode >= 400 {
-                    DispatchQueue.main.async {
-                        completion?(.failure(BaseErrorClass(message: "Network error with status code: \(statusCode)")))
-                    return
-                    }
-                }
-            }
-            
-            DispatchQueue.main.async {
-                do {
-                    let model = try JSONDecoder().decode(type, from: data)
-                    completion?(.success(model))
-                } catch {
-                    completion?(.failure(BaseErrorClass(message: error.localizedDescription)))
-                }
-            }
-            
-        }
-        task.resume()
+    
+        return urlRequest
     }
     
     override init() {}
--- iNewsXUITests/iNewsXUITests.swift
+++ iNewsXUITests/iNewsXUITests.swift
@@ -15,43 +15,31 @@ class iNewsXUITests: XCTestCase {
         super.setUp()
         continueAfterFailure = false
     }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
 
     func testCollectionView() throws {
         // UI tests must launch the application that they test.
         app.launch()
 
+        let expectation = expectation(description: "Wait for collection view cells")
         let newsCollectionView = app.collectionViews.matching(identifier: "NewsCollectionView")
         
         let collectionViewCells = newsCollectionView.cells
         XCTAssert(collectionViewCells.element.waitForExistence(timeout: 5.0))
         
         if collectionViewCells.count > 0 {
-            let count: Int = (collectionViewCells.count - 1)
             
-            let expectation = expectation(description: "Wait for collection view cells")
-            
-            for index in stride(from: 0, to: count, by: 1) {
-                let cell = collectionViewCells.element(boundBy: index)
-                XCTAssertTrue(cell.waitForExistence(timeout: 2.0), "The \(index) cell is present in collection view")
-                
-                if index == (count - 1) {
-                    expectation.fulfill()
-                }
-            }
-            
+            expectation.fulfill()
             waitForExpectations(timeout: 20, handler: nil)
             XCTAssertTrue(true, "Collection view validations finished")
+            
         } else {
             XCTAssert(false, "Could'nt find collection view cells")
         }
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
+
